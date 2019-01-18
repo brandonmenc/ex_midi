78ff755e1569c8e5b0091863e9705dfbfd5e01f0
Brandon Menc <brandon.menc@gmail.com>
2019-01-18 17:48:58 -0600

    mix new

---
+++ lib/ex_midi.ex
@@ -0,0 +1,18 @@
+defmodule ExMidi do
+  @moduledoc """
+  Documentation for ExMidi.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExMidi.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_midi_test.exs
@@ -0,0 +1,8 @@
+defmodule ExMidiTest do
+  use ExUnit.Case
+  doctest ExMidi
+
+  test "greets the world" do
+    assert ExMidi.hello() == :world
+  end
+end
